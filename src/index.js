afa704a6bac7cf76c8ac92e3cd20b90e5193eaec
MartynenkoNick <marnickmarnick87@gmail.com>
2022-01-28 15:44:10 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+//Given: number, your task is to implement function that reverse digits of this number.
+//For example:
+//reverse(123); // 321
+
 module.exports = function reverse (n) {
-  
+		num = String(num);
+		
+		let result = '';
+	
+		for(i = 0; i < num.length; i++) {
+			result = `${num[i]}${result}`;
+		};
+	
+		result = parseInt(result);
+	
+	return result;
 }
